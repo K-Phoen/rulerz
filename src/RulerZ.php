f049fdebecf20dd364a6e215549e68f637aaa22f
Kévin Gomez <contact@kevingomez.fr>
2015-01-29 21:25:20 +0000

    Add a few comments on the RulerZ class

--- src/RulerZ.php
+++ src/RulerZ.php
@@ -9,6 +9,11 @@ class RulerZ
 {
     private $executors = [];
 
+    /**
+     * Constructor.
+     *
+     * @param array $executors A list of executors to register immediatly.
+     */
     public function __construct(array $executors = [])
     {
         foreach ($executors as $executor) {
@@ -16,11 +21,26 @@ class RulerZ
         }
     }
 
+    /**
+     * Registers a new executor.
+     *
+     * @param Executor $executor The executor to register.
+     */
     public function registerExecutor(Executor $executor)
     {
         $this->executors[] = $executor;
     }
 
+    /**
+     * Filters a target using the given rule and parameters.
+     * The executor to use is determined at runtime using the registered ones.
+     *
+     * @param mixed $target     The target to filter.
+     * @param Model $rule       The rule to apply.
+     * @param array $parameters The parameters used in the rule.
+     *
+     * @return mixed The filtered target.
+     */
     public function filter($target, $rule, array $parameters = array())
     {
         $executor = $this->findExecutor($target);
@@ -29,6 +49,15 @@ class RulerZ
         return $executor->filter($target, $ast, $parameters);
     }
 
+    /**
+     * Finds an executor supporting the given target.
+     *
+     * @param mixed $target The target to filter.
+     *
+     * @throws TargetUnsupportedException
+     *
+     * @return Executor
+     */
     private function findExecutor($target)
     {
         foreach ($this->executors as $executor) {
@@ -40,6 +69,13 @@ class RulerZ
         throw new TargetUnsupportedException('The given target is not supported.');
     }
 
+    /**
+     * Parses the rule into an equivalent AST.
+     *
+     * @param string $rule The rule represented as a string.
+     *
+     * @return \Hoa\Ruler\Model
+     */
     private function parse($rule)
     {
         return Ruler::interprete($rule);
