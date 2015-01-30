1343cb1a543a3da0d02fd0991208c04efe36822e
Kévin Gomez <contact@kevingomez.fr>
2015-01-30 20:12:38 +0000

    Added specs for interpreters

---
+++ spec/Interpreter/HoaInterpreterSpec.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace spec\Interpreter;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class HoaInterpreterSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('Interpreter\HoaInterpreter');
+    }
+
+    function it_returns_an_ast_for_a_valid_rule()
+    {
+        $this->interpret('points > 30')->shouldHaveType('Hoa\Ruler\Model');
+    }
+
+    function it_throws_an_exception_for_an_invalid_rule()
+    {
+        $this->shouldThrow('Hoa\Compiler\Exception')->during('interpret', ['> and']);
+    }
+}
