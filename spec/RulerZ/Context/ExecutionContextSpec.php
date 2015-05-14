67e079ef580fd45ce49b08a1742d1d020385c070
Kévin Gomez <contact@kevingomez.fr>
2015-05-14 19:02:27 +0000

    Add specs for the ExecutionContext class

---
+++ spec/RulerZ/Context/ExecutionContextSpec.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace spec\RulerZ\Context;
+
+use PhpSpec\ObjectBehavior;
+
+class ExecutionContextSpec extends ObjectBehavior
+{
+    function let()
+    {
+        $this->beConstructedWith([
+            'some' => 'data'
+        ]);
+        $this->shouldImplement('ArrayAccess');
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('RulerZ\Context\ExecutionContext');
+    }
+
+    function it_allows_property_access_through_offset($object)
+    {
+        $this['some']->shouldReturn('data');
+    }
+
+    function it_allows_testing_property_existence($object)
+    {
+        $this->offsetExists('some')->shouldReturn(true);
+        $this->offsetExists('non_existent_offset')->shouldReturn(false);
+    }
+
+    function it_forbids_setting_properties()
+    {
+        $this->shouldThrow('\LogicException')->duringOffsetSet('foo', 'bar');
+    }
+
+    function it_forbids_unsetting_properties()
+    {
+        $this->shouldThrow('\LogicException')->duringOffsetUnset('foo');
+    }
+}
