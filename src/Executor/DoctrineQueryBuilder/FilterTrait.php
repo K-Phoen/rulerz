7adcd8dbc06736635ebae36c7a3f429c70d9e422
Kévin Gomez <contact@kevingomez.fr>
2015-07-25 21:32:05 +0000

    Dropped support for auto-joins in doctrine (for now)

--- src/Executor/DoctrineQueryBuilder/FilterTrait.php
+++ src/Executor/DoctrineQueryBuilder/FilterTrait.php
@@ -14,8 +14,14 @@ trait FilterTrait
      */
     public function filter($target, array $parameters, array $operators, ExecutionContext $context)
     {
-        // this will update the query builder
-        $this->execute($target, $operators, $parameters);
+        // this will return DQL code
+        $dql = $this->execute($target, $operators, $parameters);
+
+        // the root alias can not be determined at compile-time so placeholders are left in the DQL
+        $dql = str_replace('@@_ROOT_ALIAS_@@', $target->getRootAliases()[0], $dql);
+
+        // so we apply it to the query builder
+        $target->andWhere($dql);
 
         // now we define the parameters
         foreach ($parameters as $name => $value) {
