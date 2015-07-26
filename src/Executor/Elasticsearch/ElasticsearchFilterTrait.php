a501d183f9155d7f136e23d14e9b51dd3a6767fa
Kévin Gomez <contact@kevingomez.fr>
2015-07-26 15:34:56 +0200

    Rewrite Elasticsearch support

---
+++ src/Executor/Elasticsearch/ElasticsearchFilterTrait.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace RulerZ\Executor\Elasticsearch;
+
+use RulerZ\Context\ExecutionContext;
+
+trait FilterTrait
+{
+    abstract protected function execute($target, array $operators, array $parameters);
+
+    /**
+     * {@inheritDoc}
+     */
+    public function filter($target, array $parameters, array $operators, ExecutionContext $context)
+    {
+        /** @var array $searchQuery */
+        $searchQuery = $this->execute($target, $operators, $parameters);
+
+        /** @var \Elasticsearch\Client $target */
+        return $target->search([
+            'index' => $context['index'],
+            'type'  => $context['type'],
+            'body'  => ['query' => $searchQuery],
+        ]);
+    }
+}
