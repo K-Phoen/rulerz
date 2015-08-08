f8472c031b827fec45b3cd67e48fbc5aac85e8cc
Kévin Gomez <contact@kevingomez.fr>
2015-08-08 16:20:04 +0200

    Add a spec for the Pomm executor

---
+++ spec/RulerZ/Executor/Pomm/FilterTraitSpec.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace spec\RulerZ\Executor\Pomm;
+
+use PhpSpec\ObjectBehavior;
+use PommProject\Foundation\Where;
+use RulerZ\Context\ExecutionContext;
+use RulerZ\Stub\Executor\PommExecutorStub;
+use RulerZ\Stub\ModelStub;
+
+class FilterTraitSpec extends ObjectBehavior
+{
+    function let()
+    {
+        $this->beAnInstanceOf('RulerZ\Stub\Executor\PommExecutorStub');
+    }
+
+    function it_call_findWhere_on_the_target(ModelStub $modelStub, Where $whereClause)
+    {
+        $results = ['result'];
+
+        PommExecutorStub::$executeReturn = $whereClause;
+        $modelStub->findWhere($whereClause)->willReturn($results);
+
+        $this->filter($modelStub, $parameters = [], $operators = [], new ExecutionContext())->shouldReturn($results);
+    }
+
+    function it_call_acustom_method_if_specified_in_the_context(ModelStub $modelStub, Where $whereClause)
+    {
+        $results = ['result'];
+
+        PommExecutorStub::$executeReturn = $whereClause;
+        $modelStub->findCustom($whereClause)->willReturn($results);
+
+        $this->filter($modelStub, $parameters = [], $operators = [], new ExecutionContext([
+            'method' => 'findCustom'
+        ]))->shouldReturn($results);
+    }
+}
