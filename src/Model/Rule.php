9dadffa6473db0a11134146fdd42afbab3ebea8c
Kévin Gomez <contact@kevingomez.fr>
2015-10-17 16:41:10 +0200

    Add a way to retrieve the variable accesses from a given rule model

--- src/Model/Rule.php
+++ src/Model/Rule.php
@@ -3,7 +3,19 @@
 namespace RulerZ\Model;
 
 use Hoa\Ruler\Model as HoaModel;
+use RulerZ\Visitor\AccessCollectorVisitor;
 
 class Rule extends HoaModel\Model
 {
+    /**
+     * Returns a list of accessed variables.
+     *
+     * @return \Hoa\Ruler\Model\Bag\Context[]
+     */
+    public function getAccesses()
+    {
+        $visitor = new AccessCollectorVisitor();
+
+        return $visitor->visit($this);
+    }
 }
