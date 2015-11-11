092e94a1e0412e7fcf585e315319ce577c3fefdf
Kévin Gomez <contact@kevingomez.fr>
2015-11-11 21:52:37 +0100

    Fix travis build

--- scripts/postgres/create_database.sh
+++ scripts/postgres/create_database.sh
@@ -1,3 +1,3 @@
-docker exec -it pg-rulerz psql -U postgres -c "DROP DATABASE IF EXISTS test_rulerz"
-docker exec -it pg-rulerz psql -U postgres -c "CREATE DATABASE test_rulerz"
-docker exec -it pg-rulerz psql -U postgres -d test_rulerz -f /tmp/rulerz/examples/database.sql
+psql -U postgres -h localhost -c "DROP DATABASE IF EXISTS test_rulerz"
+psql -U postgres -h localhost -c "CREATE DATABASE test_rulerz"
+psql -U postgres -h localhost -d test_rulerz -f ./examples/database.sql
