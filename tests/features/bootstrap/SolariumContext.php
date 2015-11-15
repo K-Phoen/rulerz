ff421eeda7c6315211927a64dfad249c5db33b7b
Kévin Gomez <contact@kevingomez.fr>
2015-11-15 20:47:40 +0100

    Create a Solarium behat context

---
+++ tests/features/bootstrap/SolariumContext.php
@@ -0,0 +1,42 @@
+<?php
+
+class SolariumContext extends BaseContext
+{
+    /**
+     * @var \Solarium\Client
+     */
+    private $client;
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function initialize()
+    {
+        $this->client = new Solarium\Client([
+            'endpoint' => [
+                $_ENV['SOLR_CORE'] => [
+                    'host' => $_ENV['SOLR_HOST'],
+                    'port' => $_ENV['SOLR_PORT'],
+                    'path' => $_ENV['SOLR_PATH'],
+                    'core' => $_ENV['SOLR_CORE'],
+                ]
+            ]
+        ]);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function getCompilationTarget()
+    {
+        return new \RulerZ\Compiler\Target\Solr\SolariumVisitor();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function getDefaultDataset()
+    {
+        return $this->client->createSelect();
+    }
+}
