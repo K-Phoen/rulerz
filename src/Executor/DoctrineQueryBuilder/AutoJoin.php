7f2189ae7e2740647298c9fea80e1e0fc3527b8c
Bob D'Ercole <bobdercole@gmail.com>
2016-01-03 19:44:32 -0500

    Add Doctrine embeddable support.

--- src/Executor/DoctrineQueryBuilder/AutoJoin.php
+++ src/Executor/DoctrineQueryBuilder/AutoJoin.php
@@ -3,10 +3,18 @@
 namespace RulerZ\Executor\DoctrineQueryBuilder;
 
 use Doctrine\ORM\QueryBuilder;
+use Doctrine\ORM\EntityManager;
 
 class AutoJoin
 {
     const ALIAS_PREFIX = 'rulerz_';
+
+    /**
+     * List of root and association entity embeddables
+     *
+     * @var array
+     */
+    private $embeddables = null;
 
     /**
      * Associative list of known aliases (selected or joined tables).
@@ -40,11 +48,36 @@ class AutoJoin
 
     public function getJoinAlias($table)
     {
+        if ($this->embeddables === null) {
+            $this->embeddables = $this->analizeEmbeddables($this->queryBuilder);
+        }
+
         if ($this->joinMap === null) {
             $this->joinMap      = $this->analizeJoinedTables($this->queryBuilder);
             $this->knownAliases = array_flip($this->queryBuilder->getRootAliases()) + array_flip($this->joinMap);
 
             $this->autoJoin($this->queryBuilder);
+        }
+
+        // the table is identified as an embeddable
+        if (array_search($table, $this->embeddables) !== false) {
+            $embeddable_dimensions = explode('.', $table);
+
+            if (count($embeddable_dimensions) === 1) {
+                // the embeddable is not inside an association, so we use the root alias prefix
+                return $this->queryBuilder->getRootAlias() . '.' . $table;
+            }
+            else {
+                // remove the embeddable's property
+                array_pop($embeddable_dimensions);
+                $table_alias = implode('.', $embeddable_dimensions);
+
+                if (isset($this->knownAliases[$table_alias])) {
+                    return $table;
+                } else {
+                    return self::ALIAS_PREFIX . $table;
+                }
+            }
         }
 
         // the table name is a known alias (already join for instance) so we
@@ -59,6 +92,51 @@ class AutoJoin
         }
 
         throw new \RuntimeException(sprintf('Could not automatically join table "%s"', $table));
+    }
+
+    private function traverseAssociationsForEmbeddables(EntityManager $entityManager, array $associations, $fieldNamePrefix = false)
+    {
+        $associationsEmbeddables = array();
+
+        foreach ($associations as $association) {
+            $classMetaData = $entityManager->getClassMetadata($association['targetEntity']);
+
+            foreach ($classMetaData->embeddedClasses as $embeddedClassKey => $embeddedClass) {
+                $associationsEmbeddables[] = implode('.', array_filter(array($fieldNamePrefix, $association['fieldName'], $embeddedClassKey)));
+            }
+
+            $associationMappings = $classMetaData->getAssociationMappings();
+            $associationMappings = array_filter($associationMappings, function($associationMapping) {
+                return $associationMapping['isOwningSide'] === false;
+            });
+
+            if (count($associationMappings) !== 0) {
+                $traversedAssociationsEmbeddables = $this->traverseAssociationsForEmbeddables($entityManager, $associationMappings, $association['fieldName']);
+                $associationsEmbeddables = array_merge($associationsEmbeddables, $traversedAssociationsEmbeddables);
+            }
+        }
+
+        return $associationsEmbeddables;
+    }
+
+    private function analizeEmbeddables(QueryBuilder $queryBuilder)
+    {
+        $embeddables = array();
+        $entityManager = $queryBuilder->getEntityManager();
+        $rootEntities = $queryBuilder->getRootEntities();
+
+        foreach ($rootEntities as $rootEntity) {
+            $classMetaData = $entityManager->getClassMetadata($rootEntity);
+
+            foreach ($classMetaData->embeddedClasses as $embeddedClassKey => $embeddedClass) {
+                $embeddables[] = $embeddedClassKey;
+            }
+
+            $traversedAssociationsEmbeddables = $this->traverseAssociationsForEmbeddables($entityManager, $classMetaData->getAssociationMappings());
+            $embeddables = array_merge($embeddables, $traversedAssociationsEmbeddables);
+        }
+
+        return $embeddables;
     }
 
     /**
@@ -83,6 +161,11 @@ class AutoJoin
     private function autoJoin(QueryBuilder $queryBuilder)
     {
         foreach ($this->expectedJoinChains as $tablesToJoin) {
+            // if the table is an embeddable, the property needs to be removed
+            if (array_search(implode('.', $tablesToJoin), $this->embeddables) !== false) {
+                array_pop($tablesToJoin);
+            }
+
             // check if the first dimension is a known alias
             if (isset($this->knownAliases[$tablesToJoin[0]])) {
                 $joinTo = $tablesToJoin[0];
