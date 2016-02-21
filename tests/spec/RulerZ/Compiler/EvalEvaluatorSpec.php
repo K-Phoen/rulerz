280004bc21a59f20e8e62bc7bb5542d23d10b1a9
Kévin Gomez <contact@kevingomez.fr>
2016-02-21 22:07:51 +0100

    Fixed Compiler and Evaluator-related tests

---
+++ tests/spec/RulerZ/Compiler/EvalEvaluatorSpec.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace spec\RulerZ\Compiler;
+
+use PhpSpec\ObjectBehavior;
+
+class EvalEvaluatorSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('RulerZ\Compiler\EvalEvaluator');
+        $this->shouldHaveType('RulerZ\Compiler\Evaluator');
+    }
+
+    function it_can_evaluate_a_rule()
+    {
+        $ruleIdentifier = '(irrelevant for this evaluator)';
+        $compilerCallable = function() { return 'class WowExecutor {}'; };
+
+        $this->evaluate($ruleIdentifier, $compilerCallable);
+        $this->shouldHaveLoaded('WowExecutor');
+    }
+
+    public function getMatchers()
+    {
+        return [
+            'haveLoaded' => function ($subject, $class) {
+                return class_exists($class, false);
+            },
+        ];
+    }
+}
