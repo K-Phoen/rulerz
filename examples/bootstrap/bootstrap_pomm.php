74d5ddba6f30cb344feadaab33a815ad02ae0f0e
Kévin Gomez <kgomez@tea-ebook.com>
2017-01-25 13:51:38 +0100

    Fix Pomm example

--- examples/bootstrap/bootstrap_pomm.php
+++ examples/bootstrap/bootstrap_pomm.php
@@ -8,7 +8,7 @@ require __DIR__.'/bootstrap_general.php';
 
 $pomm = new Pomm(['test_rulerz' => [
     'dsn' => sprintf('pgsql://%s:%s@%s:%d/%s', $_ENV['POSTGRES_USER'], $_ENV['POSTGRES_PASSWD'], $_ENV['POSTGRES_HOST'], $_ENV['POSTGRES_PORT'], $_ENV['POSTGRES_DB']),
-    'class:session_builder' => '\PommProject\ModelManager\SessionBuilder',
+    'class:session_builder' => \PommProject\ModelManager\SessionBuilder::class,
 ]]);
 
 // compiler
@@ -17,7 +17,7 @@ $compiler = new \RulerZ\Compiler\Compiler(new \RulerZ\Compiler\EvalEvaluator());
 // compiled RulerZ
 $rulerz = new RulerZ(
     $compiler, [
-        new \RulerZ\Compiler\Target\Sql\Pomm(),
+        new \RulerZ\Target\Pomm\Pomm(),
     ]
 );
 
