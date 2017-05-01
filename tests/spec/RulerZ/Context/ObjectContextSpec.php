9a8312a1d7cda168c2b93fc10de19cad21bec73f
Vlad Churakov <vlchrkv@gmail.com>
2017-05-01 21:10:15 +1200

    Fix code style.

--- tests/spec/RulerZ/Context/ObjectContextSpec.php
+++ tests/spec/RulerZ/Context/ObjectContextSpec.php
@@ -6,13 +6,13 @@ use PhpSpec\ObjectBehavior;
 
 class ObjectContextSpec extends ObjectBehavior
 {
-    function let(\stdClass $object)
+    public function let(\stdClass $object)
     {
         $this->beConstructedWith($object);
         $this->shouldImplement('ArrayAccess');
     }
-
-    function it_is_initializable()
+    
+    public function it_is_initializable()
     {
         $this->shouldHaveType('RulerZ\Context\ObjectContext');
     }
@@ -23,28 +23,28 @@ class ObjectContextSpec extends ObjectBehavior
 
         $this['property']->shouldReturn(42);
     }
-
-    function it_doesnt_wrap_nulls_with_self($object)
+    
+    public function it_doesnt_wrap_nulls_with_self($object)
     {
         $object->property = null;
 
         $this['property']->shouldReturn(null);
     }
-
-    function it_allows_testing_property_existence($object)
+    
+    public function it_allows_testing_property_existence($object)
     {
         $object->property = 42;
 
         $this->offsetExists('property')->shouldReturn(true);
         $this->offsetExists('non_existent_property')->shouldReturn(false);
     }
-
-    function it_forbids_setting_properties()
+    
+    public function it_forbids_setting_properties()
     {
         $this->shouldThrow('\RuntimeException')->duringOffsetSet('foo', 'bar');
     }
-
-    function it_forbids_unsetting_properties()
+    
+    public function it_forbids_unsetting_properties()
     {
         $this->shouldThrow('\RuntimeException')->duringOffsetUnset('foo');
     }
